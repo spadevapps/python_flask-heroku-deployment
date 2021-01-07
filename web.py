3704e775e78a1abc518cb4ea9cd64d11b4a1f4e2
spadevaps <spappl19@g.holycross.edu>
2021-01-06 19:48:16 -0500

    try again2

--- web.py
+++ web.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/WikiWebsite/HomepageTemplate/index.html")
+@app.route("/")
 def main():
-    return render_template('index.html')
+    return render_template('HomepageTemplate/index.html')
 
 if __name__ == "__main__":
     app.run(host="localhost", port=8080, debug=True)
